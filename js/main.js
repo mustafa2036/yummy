a9fdc79c22ec4014780918695cf6444117e32528
mustafa2036 <missem704@gmail.com>
2023-01-30 09:18:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// let swiper = new Swiper(".mySwiper", {
+//     loop: true,
+//     slidesPerView: 1,
+//     spaceBetween: 0,
+//     slidesPerView:1,
+//     speed:2000,
+//     effect: 'slide',
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+// });
+  
+// let navbar = document.querySelector('.navbar');
+
+// window.onscroll = () => {
+//     (scrollY > 100)?navbar.classList.add('active'): navbar.classList.remove('active');
+// }
+// $(document).ready(function () {
+//   $('.close').click(()=>{
+//     $(".contact").css("top","-100%");
+//   })
+//   $('.fa-user').click(()=>{
+//     $(".contact").css("top","0");
+//   });
+//   $(".sk-circle").fadeOut(3000,()=>{
+//     $("#loading").fadeOut(2000, ()=>{
+//       $("body").css('overflow','auto');
+//     })
+//   });
+// });
+
+// // AOS.init();
